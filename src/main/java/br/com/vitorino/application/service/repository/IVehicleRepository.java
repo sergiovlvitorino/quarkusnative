753ac0a35cf59a48d1788259799526650c821920
Sergio Vitorino <sergiovlvitorino@gmail.com>
2022-05-15 10:58:47 -0300

    refactoring

--- src/main/java/br/com/vitorino/application/service/repository/IVehicleRepository.java
+++ src/main/java/br/com/vitorino/application/service/repository/IVehicleRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface IVehicleRepository extends JpaRepository<VehicleEntity, UUID> {
+interface IVehicleRepository extends JpaRepository<VehicleEntity, UUID> {
 }
--- src/main/java/br/com/vitorino/infrastructure/visitor/Host.java
+++ src/main/java/br/com/vitorino/infrastructure/visitor/Host.java
@@ -1,7 +1,5 @@
 package br.com.vitorino.infrastructure.visitor;
 
-import br.com.vitorino.infrastructure.visitor.Visitor;
-
 public interface Host {
 
     default void receive(Visitor visitor) {
